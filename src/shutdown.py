c28716d284b6718abe05cd7c7922093450964f59
Jason Burgess <jason@notplugged.in>
2015-09-25 01:50:51 -0500

    Updated shutdown.py.

--- src/shutdown.py
+++ src/shutdown.py
@@ -16,10 +16,13 @@ gpio.setmode(gpio.BOARD);
 # Setup pin 7 as input
 gpio.setup(7, gpio.IN, pull_up_down=gpio.PUD_DOWN);
 
-if (gpio.input(7) == 0) {
+if (gpio.input(7) == 0):
   print "[SHUTDOWN] Switch is not set to on. Waiting for on state."
   gpio.wait_for_edge(7, gpio.RISING);
-}
+
+# Reset pin 7 configuration
+gpio.clanup(7);
+gpio.setup(7, gpio.IN, pull_up_down=gpio.PUD_DOWN);
 
 # Ensures we have an off button connected first as well
 # Setup an interrupt to detect the off button press
